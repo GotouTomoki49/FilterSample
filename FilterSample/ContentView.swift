46193e5499ff6874baacb3671560a3dc5e5086ef
GotouTomoki49 <21cm0149@jec.ac.jp>
2022-06-28 09:37:03 +0900

    Initial Commit

---
+++ FilterSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FilterSample
+//
+//  Created by cmStudent on 2022/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
