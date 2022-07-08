d3b5bf4d14f72734c5b5377c67ef46eb90368153
GotouTomoki49 <21cm0149@jec.ac.jp>
2022-07-08 10:34:04 +0900

    commit

--- FilterSample/Content/FilterBanner/FilterBannerView.swift
+++ FilterSample/Content/FilterBanner/FilterBannerView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct FilterBannerView: View {
+    @State var selectedFilter: FilterType? = nil
+    
     var body: some View {
-        Text("Hello,World")
+        VStack{
+            FilterTitleView(title: nil)
+            FilterIconContainerView(
+                selectedFIlter: $selectedFilter)
+            FilterButtonContainerView()
+        }
+        
     }
 }
 
 struct FilterBannerView_Previews: PreviewProvider {
     static var previews: some View {
-        FilterImage(filterType: .gaussianBlur)
+        FilterBannerView()
+        
     }
 }
 
@@ -36,10 +45,12 @@ struct FilterImage: View{
     //何のフィルターか
     let filterType: FilterType
     
-    let uiImage: UIImage = UIImage(named:"icon")!
+    @Binding var selectedFilter:FilterType?
+    
+    let uiImage: UIImage = UIImage(named:"image")!
     var body: some View{
         Button{
-            //処理
+            selectedFilter = filterType
         }label: {
             image?
                 .resizable()
@@ -56,5 +67,64 @@ struct FilterImage: View{
             }
         }
         
+    }
+}
+
+struct FilterIconContainerView: View{
+    @Binding var selectedFIlter: FilterType?
+    var body: some View{
+        ScrollView(.horizontal,showsIndicators: false){
+            //View
+            HStack{
+                //アイコンを並べる
+                //モザイク
+                FilterImage(filterType: .pixellate,
+                            selectedFilter: $selectedFIlter)
+                //セピア
+                FilterImage(filterType: .sepiaTone ,
+                            selectedFilter: $selectedFIlter)
+                //シャープ
+                FilterImage(filterType: .sharpenLuminance,
+                            selectedFilter: $selectedFIlter)
+                //モノクロ
+                FilterImage(filterType: .photoEffectMono,
+                            selectedFilter: $selectedFIlter)
+                //ぼかし
+                FilterImage(filterType: .gaussianBlur,
+                            selectedFilter: $selectedFIlter)
+                
+            }
+            .padding([.leading,.trailing],16)
+        }
+    }
+}
+
+struct FilterButtonContainerView: View{
+    var body: some View{
+        HStack{
+            
+            Button{
+                //閉じる処理
+                
+            }label:{
+                Image(systemName: "xmark")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20)
+                    .padding()
+            }
+            Spacer()
+            
+            Button{
+                //確定する処理
+                
+            }label:{
+                Image(systemName: "checkmark")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20)
+                    .padding()
+            }
+        }
     }
 }
