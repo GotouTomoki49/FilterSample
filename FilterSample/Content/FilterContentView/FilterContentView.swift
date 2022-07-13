e82cb100f149723f25efb527ff62b57b61ed7849
GotouTomoki49 <21cm0149@jec.ac.jp>
2022-07-13 17:56:01 +0900

    Commit

--- FilterSample/Content/FilterContentView/FilterContentView.swift
+++ FilterSample/Content/FilterContentView/FilterContentView.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct FilterContentView: View {
-    
     @StateObject private var viewModel = FilterContentViewModel()
-    
     var body: some View {
         NavigationView {
             ZStack {
@@ -26,57 +24,74 @@ struct FilterContentView: View {
                 } else {
                     EmptyView()
                 }
-                FilterBannerView(isShowBanner: $viewModel.isShowBanner,applyingFilter: $viewModel.applyingFilter)
+                FilterBannerView(isShowBanner: $viewModel.isShowBanner, applyingFilter: $viewModel.applyingFilter)
                 
-            }// ZStackの終わり
-            // Navigation Titleの設定
-            .navigationTitle("ふぃるたーくん")
-            // Navigation Item
-            // trailingにHStackでButtonを２つ置く
+            }
+            .navigationTitle("フィルター")
             .navigationBarItems(trailing: HStack {
-                Button {} label: {
+                Button {
+                    viewModel.apply(.tappedSaveIcon)
+                } label: {
                     Image(systemName: "square.and.arrow.down")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
                 }
-                Button {} label: {
+                
+                Button {
+                    viewModel.apply(.tappedActionSheet(selectType: .photoLibrary))
+                } label: {
                     Image(systemName: "photo")
                 }
+                
             })
-            .onAppear{
+            .onAppear {
                 // 画面表示時の処理
                 viewModel.apply(.onAppear)
             }
+            
             .actionSheet(isPresented: $viewModel.isShowActionSheet) {
                 actionSheet
             }
-            .sheet(isPresented: $viewModel.isShowImagePickerView) {
+            
+            // Bindingは＄をつけないと値渡しになってしまう＄を使うことで参照になる
+            .sheet(isPresented: $viewModel.isShowImagePickerView){
                 ImagePicker(isShown: $viewModel.isShowImagePickerView, image: $viewModel.image, sourceType: viewModel.selectedSourceType)
             }
+            
+            .alert(isPresented: $viewModel.isShowAlert) {
+                Alert(title: Text(viewModel.alertTitle))
+            }
+            
         }
     }
     
     var actionSheet: ActionSheet {
         var buttons: [ActionSheet.Button] = []
+        
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             // カメラボタン
-            let cameraButton = ActionSheet.Button.default(Text("写真を撮る")){
-                viewModel.apply(.tappedActionSheet(selectType: .camera))
-            }
+            let cameraButton = ActionSheet.Button
+                .default(Text("写真を撮る")){
+                    viewModel.apply(.tappedActionSheet(selectType: .camera))
+                }
             buttons.append(cameraButton)
         }
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            // フォトライブラリー
-            let photoLibraryButton = ActionSheet.Button.default(Text("アルバムから選択")) {
-                viewModel.apply(.tappedActionSheet(selectType: .photoLibrary))
-            }
+            // フォトライブラリーボタン
+            let photoLibraryButton = ActionSheet.Button
+                .default(Text("アルバムから選択")){
+                    viewModel.apply(.tappedActionSheet(selectType: .photoLibrary))
+                }
             buttons.append(photoLibraryButton)
         }
         // キャンセルボタン
-        let cancelButton = ActionSheet.Button.cancel(Text("キャンセル"))
+        let cancelButton = ActionSheet.Button.cancel(Text("キャンセル")){}
         buttons.append(cancelButton)
         
-        let actionSheet = ActionSheet(title: Text("画像を選択する"), message: nil, buttons: buttons)
+        let actionSheet = ActionSheet(title: Text("画像選択"),
+                                      message: nil, buttons: buttons)
         return actionSheet
-        
     }
 }
 
--- FilterSample/Content/FilterContentView/FilterContentViewModel.swift
+++ FilterSample/Content/FilterContentView/FilterContentViewModel.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 import Combine
 
-final class FilterContentViewModel: ObservableObject {
+final class FilterContentViewModel: NSObject, ObservableObject {
     enum Inputs {
         case onAppear
         case tappedActionSheet(selectType: UIImagePickerController.SourceType)
+        case tappedSaveIcon
     }
+    
     @Published var image: UIImage?
     @Published var filterdImage: UIImage?
     
@@ -21,53 +23,76 @@ final class FilterContentViewModel: ObservableObject {
     
     @Published var selectedSourceType: UIImagePickerController.SourceType = .camera
     
-    // フィルターバナー表示用フラグ
+    // フィルターバナー表示のフラッグ
     @Published var isShowBanner = false
     
     // フィルターを適用するFilterType
     @Published var applyingFilter: FilterType? = nil
     
-    // Combineを実行するためのCancellable
+    // Combineを実行するためのcancellable
     var cancellables: [Cancellable] = []
     
-    init() {
-        // $を付けている→状態変数(Published→Publisher)
+    var alertTitle: String = ""
+    // アラートwp表示するためのフラグ
+    @Published var isShowAlert = false
+    
+    override init() {
+        super.init()
+        // $を付けている（状態変数として使う→今回はPublished→Publisher）
         let imageCancellable = $image.sink { [weak self] uiimage in
-            guard let self = self, let uiimage = uiimage else { return }
+            guard let self = self , let uiimage = uiimage else { return }
             
             self.filterdImage = uiimage
         }
-        //applyingFilterが更新
-        let filterCancellable = $applyingFilter.sink{ [weak self]FilterType in
+        
+        // applyingFilterが更新されたら
+        
+        let filterCancellable = $applyingFilter.sink { [weak self] filterType in
             guard let self = self,
-                  let filterType = FilterType,
-                  let image = self.image else{
+                  let filterType = filterType,
+                  let image = self.image else {
                 return
             }
             
-            //画像を加工する
-            guard let filterdUImage = self.updateImage(with: image, type: filterType) else {return}
-            self.filterdImage = filterdUImage
+            
+            // 画像加工
+            guard let filterdUIImage = self.updateImage(with: image, type: filterType) else { return }
+            self.filterdImage = filterdUIImage
+            //cancellables.append(filterCancellable)
+            
         }
         
         cancellables.append(imageCancellable)
         cancellables.append(filterCancellable)
     }
-    private func updateImage(with image:UIImage, type filter: FilterType) -> UIImage?{
+    
+    // うまくいくか分からないのでオプショナルにする　＝　？をつける
+    private func updateImage(with image: UIImage, type filter: FilterType) -> UIImage? {
         return filter.filter(inputImage: image)
     }
+    
     
     func apply(_ inputs: Inputs) {
         switch inputs {
         case .onAppear:
             if image == nil {
+                // アクションシートを表示したい
                 isShowActionSheet = true
             }
         case .tappedActionSheet(let sourceType):
-            
-            // フォトライブラリーを起動orカメラを起動
+            // フォトライブラリーを起動する（あるいはカメラを起動する？）
             selectedSourceType = sourceType
             isShowImagePickerView = true
+        case .tappedSaveIcon:
+            // 画像を保存する処理
+            UIImageWriteToSavedPhotosAlbum(filterdImage!, self, #selector(imageSaveCompletion(_:didFinishSaveingWithError:contextInfo:)), nil)
+            //break
+            
         }
+    }
+    @objc func imageSaveCompletion(_ image: UIImage, didFinishSaveingWithError error: Error? ,
+                                   contextInfo: UnsafeRawPointer) {
+        alertTitle = error == nil ? "画像が保存されました" : error?.localizedDescription ?? ""
+        isShowAlert = true
     }
 }
